00313ffd86ea0295aa308216495e0e140eb37b59
Martin Reinoso <reinosom@unimelb.edu.au>
2020-07-18 19:07:16 +1000

    Initial Commit

---
+++ Assets/changeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+	public bool ChangeColorRed = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter ()
+    {
+    	if (ChangeColorRed == true){
+    		gameObject.GetComponent<Renderer>().material.color = Color.red;
+    	} else{
+    		gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    	}
+    }
+}
